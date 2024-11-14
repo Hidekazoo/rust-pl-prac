e579468e7fc6d700cce3e501f83ede31f5cb7f3d
hidekazoo <hidekazoo.crudzoo@gmail.com>
2024-11-14 21:48:06 +0900

    add polars

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use polars::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+   let df = df![
+       "names" => ["a","b","c"],
+       "valus" => [1,2,3],
+       "values_null"=> [Some(1), None, None]
+   ];
+    println!("{:?}", df.ok());
 }
